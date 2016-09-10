c45f6ba4ede6d372c8a06a02609c38bb528fdf74
Cadey Dodrill <me@christine.website>
2016-09-10 16:27:52 -0700

    database: fic types

--- database/database.go
+++ database/database.go
@@ -3,20 +3,25 @@ package database
 import (
 	"context"
 	"errors"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"cloud.google.com/go/datastore"
 	"github.com/Xe/hideyhole-site/interop"
+	"github.com/bwmarrin/snowflake"
 )
 
 // Errors
 var (
 	ErrNoUserFound = errors.New("database: no user found")
+	ErrNoFicFound  = errors.New("database: no fic found")
 )
 
 type Database struct {
-	ds  *datastore.Client
-	ctx context.Context
+	ds    *datastore.Client
+	ctx   context.Context
+	IDGen *snowflake.Node
 }
 
 //hack
@@ -34,9 +39,15 @@ func Init(namespace, projectID string) (*Database, error) {
 		return nil, err
 	}
 
+	node, err := snowflake.NewNode(rand.Int63() % 1024)
+	if err != nil {
+		return nil, err
+	}
+
 	db := &Database{
-		ds:  client,
-		ctx: ctx,
+		ds:    client,
+		ctx:   ctx,
+		IDGen: node,
 	}
 
 	return db, err
@@ -47,6 +58,7 @@ func (d *Database) GetUser(id string) (*interop.DiscordUser, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer tx.Rollback()
 
 	dUser, _, err := d.getUser(d.ctx, tx, id)
 	if err != nil {
@@ -96,6 +108,7 @@ func (d *Database) PutUser(dUser *interop.DiscordUser) error {
 	if err != nil {
 		return err
 	}
+	defer tx.Rollback()
 
 	_, err = d.putUser(d.ctx, tx, dUser)
 	if err != nil {
@@ -122,4 +135,99 @@ func (d *Database) putUser(ctx context.Context, tx *datastore.Transaction, dUser
 	}
 
 	return tx.Put(key, dUser)
+}
+
+type Fic struct {
+	ID         string
+	Created    time.Time
+	Edited     time.Time
+	AuthorID   string
+	Title      string
+	ChapterIDs []string
+
+	Description     string `datastore:",noindex"`
+	DescriptionHTML string `datastore:",noindex"`
+}
+
+func (d *Database) GetFic(id string) (*Fic, error) {
+	tx, err := d.ds.NewTransaction(d.ctx, datastore.MaxAttempts(1))
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	fic, _, err := d.getFic(d.ctx, tx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return fic, nil
+}
+
+func (d *Database) getFic(ctx context.Context, tx *datastore.Transaction, id string) (*Fic, *datastore.Key, error) {
+	results := []Fic{}
+	var result *Fic
+	var resultKey *datastore.Key
+
+	q := datastore.NewQuery("Fic").
+		Filter("ID =", id).
+		Limit(1).
+		Transaction(tx)
+
+	resultKeys, err := d.ds.GetAll(ctx, q, results)
+	if err != nil {
+		switch err {
+		case datastore.ErrNoSuchEntity:
+			return nil, nil, ErrNoFicFound
+		default:
+			return nil, nil, err
+		}
+	}
+
+	result = &results[0]
+	resultKey = resultKeys[0]
+
+	return result, resultKey, nil
+}
+
+func (d *Database) putFic(ctx context.Context, tx *datastore.Transaction, fic *Fic) (*datastore.PendingKey, error) {
+	_, key, err := d.getFic(ctx, tx, fic.ID)
+	if err != nil && err != ErrNoFicFound {
+		return nil, err
+	}
+
+	if key == nil {
+		key = datastore.NewIncompleteKey(ctx, "Fic", nil)
+	}
+
+	return tx.Put(key, fic)
+}
+
+type Chapter struct {
+	ID       string
+	FicID    string
+	AuthorID string
+	Title    string
+	Created  time.Time
+
+	Content     string `datastore:",noindex"`
+	ContentHTML string `datastore:",noindex"`
+}
+
+func (d *Database) addFicChapter(ctx context.Context, tx *datastore.Transaction, fic *Fic, ficKey *datastore.Key, chapter *Chapter) (*datastore.PendingKey, error) {
+	fic.ChapterIDs = append(fic.ChapterIDs, chapter.ID)
+
+	_, err := d.putFic(ctx, tx, fic)
+	if err != nil {
+		return nil, err
+	}
+
+	key := datastore.NewIncompleteKey(ctx, "Chapter", ficKey)
+
+	return tx.Put(key, chapter)
 }
